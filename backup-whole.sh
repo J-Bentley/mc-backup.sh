1770f6cc7e41ee4ed81a30b5ad6b8e6f4fbd83e2
Jordan Bentley <JordanBentley77@gmail.com>
2018-12-12 16:03:20 -0500

    Update backup-whole.sh

--- backup-whole.sh
+++ backup-whole.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
+# Use crontab to auto schedule
 # Uses "stuff" to inject strings into the input buffer of a running window.
-# Says a warning in-game, saves worlds, stops server & compresses backup.
 
-screen -p 0 -X stuff "say Treescape restarting in 2 mins!$(printf \\r)"
-sleep 2m
-
-screen -p 0 -X stuff "save-all$(printf \\r)"
-sleep 5
-
-screen -p 0 -X stuff "stop$(printf \\r)"
-sleep 10
+# Variables - Change to your needs
+fileToBackup=$("/home/jordan/treescape")
+backupLocation=$("/home/jordan/BACKUP/")
+serverName=$("Treescape")
+startScript=$("bash start.sh")
 
 current_day=$(date +"%m_%d_%Y")
-tar -czPf /home/jordan/BACKUP/treescape-$current_day.tar.gz /home/jordan/treescape/
-# Compress /treescape/ & save it to working directory [?] (Make it save to /backup/)
 
-screen -p 0 -X stuff "bash start.sh$(printf \\r)"
-# Restart server (not sure if will work)
+# In-game warnings, wait 2 mins, stop and echo to screen. Waits between saving, stopping and compressing for HDDs?
+screen -p 0 -X stuff "say $serverName is restarting in 2 mins!$(printf \\r)"
+sleep 1m
+screen -p 0 -X stuff "say $serverName is restarting in 1 min!!$(printf \\r)"
+sleep 1m
+screen -p 0 -X stuff "say $serverName is restarting!$(printf \\r)"
+screen -p 0 -X stuff "save-all$(printf \\r)"
+sleep 5
+screen -p 0 -X stuff "stop$(printf \\r)"
+sleep 5
+screen -p 0 -X stuff "echo WORLDS SAVED AND SERVER STOPPED. COMPRESSING BACKUP...$(printf \\r)"
+
+tar -czPf $backupLocation-$current_day.tar.gz $fileToBackup
+
+screen -p 0 -X stuff "echo BACKUP COMPLETE! RESTARTING SERVER...$(printf \\r)"
+screen -p 0 -X stuff "$startScript $(printf \\r)"
