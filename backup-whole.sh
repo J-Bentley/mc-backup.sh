abfa7382d1d7942e1dc69c191361b970b1e39ca6
Jordan Bentley <JordanBentley77@gmail.com>
2018-12-28 21:00:08 -0500

    Update backup-whole.sh

--- backup-whole.sh
+++ backup-whole.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 # Jordan Bentley 2018-12-15
-# A Minecraft server backup/compression script that gracefully stops the server, with in-game warnings to players.
-# Uses stuff to inject strings into the input buffer of a running window, assumes only 1 running screen session(!)
-# Uses GDRIVE to upload compressed folder to google drive folder specified.
+# A Minecraft server backup/compression script that gracefully stops the server, with in-game warnings to players
+# Uses stuff to inject strings into the input buffer of a running window, assumes only running screen session
 
-# Variables - Change to your needs!
+# Variables - Change to your needs
 fileToBackup="/home/jordan/treescape"
 backupLocation="/home/jordan/BACKUP/"
-gdriveFolderID="1GkyWWtwF2dbKFo29E1GC9y1VVhj_UuFG" # "gdrive list" to get folderID's
 serverName="Treescape"
 startScript="bash start.sh"
 
-# A unique identifier for the file-name. Change if creating backup more than once a day!
+if [ ! -f $fileToBackup ]; then
+    echo "fileToBackup not found!"
+    exit 1
+fi
+
+if [ ! -f $backupLocation ]; then
+    echo "backupLocation not found!"
+    exit 1
+fi
+
+# A unique identifier for file-name - change if creating backup more than once a day
 currentDay=$(date +"%m_%d_%Y")
 
-screen -p 0 -X stuff "say $serverName is restarting in 2 mins!$(printf \\r)"
+echo "Backup/Compression Script Initiated on $currentDay."
+
+screen -p 0 -X stuff "say $serverName is restarting in 2 minutes!$(printf \\r)"
 sleep 1m
-screen -p 0 -X stuff "say $serverName is restarting in 1 min!!$(printf \\r)"
+screen -p 0 -X stuff "say $serverName is restarting in 1 minute!!$(printf \\r)"
 sleep 1m
-screen -p 0 -X stuff "say $serverName is restarting!$(printf \\r)"
+screen -p 0 -X stuff "say $serverName is restarting now!$(printf \\r)"
 screen -p 0 -X stuff "save-all$(printf \\r)"
 sleep 5
 screen -p 0 -X stuff "stop$(printf \\r)"
@@ -27,11 +37,12 @@ screen -p 0 -X stuff "echo Worlds saved and $serverName stopped. $(printf \\r)"
 
 screen -p 0 -X stuff "echo Compressing backup on $currentDay...$(printf \\r)"
 tar -czPf $backupLocation$serverName-$currentDay.tar.gz $fileToBackup
-screen -p 0 -X stuff "echo Compression complete!
-
-# screen -p 0 -X stuff "echo Uploading compressed backup to Gdrive on $currentDay... $(printf \\r)"
-# gdrive upload -p $gdriveFolderID $backupLocation$serverName-$currentDay.tar.gz
-# screen -p 0 -X stuff "echo Upload complete! $(printf \\r)"
+screen -p 0 -X stuff "echo Compression complete!"
 
 screen -p 0 -X stuff "echo Restarting $serverName... $(printf \\r)"
 screen -p 0 -X stuff "$startScript $(printf \\r)"
+
+echo "$fileToBackup compressed to $backupLocation:"
+ls -a $backupLocation
+
+exit 0
