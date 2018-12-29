4789f13887f62cdb4def59a2984473654b56ba81
Jordan Bentley <JordanBentley77@gmail.com>
2018-12-29 12:13:25 -0500

    Update backup-whole.sh

--- backup-whole.sh
+++ backup-whole.sh
@@ -4,25 +4,25 @@
 # Uses stuff to inject strings into the input buffer of a running window, assumes only running screen session
 
 # Variables - Change to your needs
-fileToBackup="/home/jordan/treescape"
+fileToBackup="/home/jordan/treescape/"
 backupLocation="/home/jordan/BACKUP/"
 serverName="Treescape"
 startScript="bash start.sh"
 
-if [ ! -f $fileToBackup ]; then
+# A unique identifier for file-name - change if creating backup more than once a day
+currentDay=$(date +"%m-%d-%Y")
+
+if [ ! -d $fileToBackup ]; then
     echo "fileToBackup not found!"
     exit 1
 fi
 
-if [ ! -f $backupLocation ]; then
+if [ ! -d $backupLocation ]; then
     echo "backupLocation not found!"
     exit 1
 fi
 
-# A unique identifier for file-name - change if creating backup more than once a day
-currentDay=$(date +"%m_%d_%Y")
-
-echo "Backup/Compression Script Initiated on $currentDay."
+echo "Backup/Compression of $serverName Initiated on $currentDay..."
 
 screen -p 0 -X stuff "say $serverName is restarting in 2 minutes!$(printf \\r)"
 sleep 1m
@@ -30,19 +30,16 @@ screen -p 0 -X stuff "say $serverName is restarting in 1 minute!!$(printf \\r)"
 sleep 1m
 screen -p 0 -X stuff "say $serverName is restarting now!$(printf \\r)"
 screen -p 0 -X stuff "save-all$(printf \\r)"
-sleep 5
+sleep 5 # remove if you live like larry
 screen -p 0 -X stuff "stop$(printf \\r)"
-sleep 5 # waits for hdds, reduce/remove if desired
-screen -p 0 -X stuff "echo Worlds saved and $serverName stopped. $(printf \\r)"
+sleep 5 
+screen -p 0 -X stuff "echo $serverName stopped! Compressing [$fileToBackup] to [$backupLocation] on [$currentDay] ...$(printf \\r)"
 
-screen -p 0 -X stuff "echo Compressing backup on $currentDay...$(printf \\r)"
 tar -czPf $backupLocation$serverName-$currentDay.tar.gz $fileToBackup
-screen -p 0 -X stuff "echo Compression complete!"
-
-screen -p 0 -X stuff "echo Restarting $serverName... $(printf \\r)"
+screen -p 0 -X stuff "echo Compression complete! Restarting $serverName... $(printf \\r)"
 screen -p 0 -X stuff "$startScript $(printf \\r)"
 
-echo "$fileToBackup compressed to $backupLocation:"
+echo "[$fileToBackup] compressed to [$backupLocation]:"
 ls -a $backupLocation
 
 exit 0
