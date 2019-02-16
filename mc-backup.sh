4669cd445f51e9f21b6ee162bffae1956663ff81
Jordan Bentley <JordanBentley77@gmail.com>
2019-02-16 16:01:43 -0500

    Update mc-backup.sh

--- mc-backup.sh
+++ mc-backup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # Jordan Bentley 2018-12-15
-# A compression script that gracefully stops a Minecraft server running on Screen, with in-game warnings to players and backup methods.
+# A compression script that gracefully stops a Minecraft server running on Ubuntu within a Screen session, with in-game warnings to players and external backup methods
+# Dependancies: screen, tput, tar, gzip, stuff
 
-#--------- Change these ---------
+# User Configuration Variables -- change these!
 fileToBackup="/home/me/MinecraftServer"
 backupLocation="/home/me/Backup"
 serverName="MyServer"
 startScript="bash start.sh"
-serverWorlds=("world" "world_nether" "world_the_end")
-gdrivefolderid="NotSet"
-currentDay=$(date +"%m-%d-%Y")
-graceperiod="1m"
-#---------------------------------
+serverWorlds=("world" "world_nether" "world_the_end") #add your non vanilla world names here seperated by space ex. "lobby" "creative" "arena"
+gdrivefolderid="NotSet" #use "gdrive list" to find the gdrive folder ID you wish to upload to and paste it here, not required if not using gdrive
+currentDay=$(date +"%m-%d-%Y") #unique file identifier, add %H if running more than once a day to avoid duplicates
+graceperiod="1m" #time to wait between in-game warnings to players of restart
+#---------------
 
 gdriveUpload=false
 worldsOnly=false
@@ -19,9 +20,11 @@ worldUpload=false
 restartOnly=false
 
 screens=$(ls /var/run/screen/S-$USER -1 | wc -l || 0)
+serverRunning=true
 
 bold=$(tput bold)
 normal=$(tput sgr0)
+#for bolding output in cli
 
 stopHandling () {
   screen -p 0 -X stuff "say $serverName is restarting in $graceperiod!$(printf \\r)"
@@ -34,28 +37,28 @@ stopHandling () {
 }
 
 gdrivefoldercheck () {
-  if ! ps -e | grep -q "gdrive"; then
-    echo "Gdrive not installed! (No Gdrive process found)"
+  if ! ps -e | grep -q "gdrive"; then # probably doesnt work
+    echo "Gdrive not installed or running!"
     exit 1
-  elif ! gdrive list | grep -q "$gdrivefolderid"; then
-    echo "Gdrive folder ID ($gdrivefolderid) not found or not installed!"
+  elif ! gdrive list | grep -q "$gdrivefolderid"; then # greps for gdrivefolderid from gdrive list
+    echo "Gdrive folder ID ($gdrivefolderid) not found!"
     exit 1
   fi
 }
 
 if [ ! -d $fileToBackup ]; then
-    echo "fileToBackup not found!"
+    echo "Minecraft server folder not found! ($fileToBackup)"
     exit 1
 fi
 
 if [ ! -d $backupLocation ]; then
-    echo "backupLocation not found!"
+    echo "Backup folder not found! ($backupLocation)"
     exit 1
 fi
 
 if ! ps -e | grep -q "java"; then
-    echo "$serverName is not running! (No Java process found)"
-    exit 1
+    echo "$serverName is not running! Continuing without in-game warnings..."
+    serverRunning=false
 fi
 
 if [ $screens -eq 0 ]; then
@@ -63,7 +66,6 @@ if [ $screens -eq 0 ]; then
     exit 1
 elif [ $screens -gt 1 ]; then
     echo "More than 1 screen session is running, am confuse!"
-    screen -ls
     exit 1
 fi
 
@@ -90,7 +92,7 @@ while [ $# -gt 0 ]; do
       ;;
     *)
       echo -e "${bold}Invalid argument: ${normal}"$1 
-      echo -e "Usage:\nNo args | Compress $serverName's root dir to backup dir\n-h | Help (this)\n-w | Compress worlds only\n-r | Restart with warnings, no backups made.\n-g | Compress & upload $serverName's root dir to gdrive\n-wg | Compress & upload worlds to gdrive\n"
+      echo -e "Usage:\nNo args | Compress $serverName's root dir to backup dir\n-h | Help\n-w | Compress worlds only\n-r | Restart with warnings, no backups made.\n-g | Compress & upload $serverName's root dir to gdrive\n-wg | Compress & upload worlds to gdrive\n"
       exit 1
       ;;
   esac
@@ -100,10 +102,13 @@ done
 echo -e "\n${bold}MC-BACKUP by Arcaniist${normal}\n---------------------------\nA compression/backup script of\n[$fileToBackup] to [$backupLocation] for $serverName!\n"
 echo "Starting backup on $currentDay..."
 elapsedTimeStart="$(date -u +%s)"
-stopHandling
+
+if $serverRunning; then
+    stopHandling
+fi
 
 if $restartOnly; then
-    screen -p 0 -X stuff "echo $serverName stopped!$(printf \\r)"
+    screen -p 0 -X stuff "echo $serverName stopped! Restarting $serverName...$(printf \\r)"
 elif $worldsOnly; then
     screen -p 0 -X stuff "echo $serverName stopped! Compressing [$fileToBackup/worlds] to [$backupLocation] on [$currentDay]...$(printf \\r)"
     tar cf $backupLocation/$serverName[WORLDS]-$currentDay.tar --files-from /dev/null
@@ -112,7 +117,7 @@ elif $worldsOnly; then
         tar rf $backupLocation/$serverName[WORLDS]-$currentDay.tar "$fileToBackup/$item"
     done
     gzip $backupLocation/$serverName[WORLDS]-$currentDay.tar
-    screen -p 0 -X stuff "echo Compression complete!$(printf \\r)"
+    screen -p 0 -X stuff "echo Compression complete! Restarting $serverName...$(printf \\r)"
 elif $worldUpload; then
     screen -p 0 -X stuff "echo $serverName stopped! Compressing [$fileToBackup/worlds*] to [$backupLocation] on [$currentDay] then uploading to Gdrive...$(printf \\r)"
     tar cf $backupLocation/$serverName[WORLDS]-$currentDay.tar --files-from /dev/null
@@ -124,30 +129,28 @@ elif $worldUpload; then
     screen -p 0 -X stuff "echo Compression complete! Uploading to Gdrive... $(printf \\r)"
 
     gdrive upload -p $gdrivefolderid $backupLocation/$serverName[WORLDS]-$currentDay.tar
-    screen -p 0 -X stuff "echo Upload complete! $(printf \\r)"
+    screen -p 0 -X stuff "echo Upload complete! Restarting $serverName...$(printf \\r)"
 else
     screen -p 0 -X stuff "echo $serverName stopped! Compressing [$fileToBackup/] to [$backupLocation/] on [$currentDay]...$(printf \\r)"
     tar -czPf $backupLocation/$serverName-$currentDay.tar.gz $fileToBackup
-    screen -p 0 -X stuff "echo Compression complete!$(printf \\r)"
+    screen -p 0 -X stuff "echo Compression complete! Restarting $serverName...$(printf \\r)"
 fi
 
 if $gdriveUpload; then
     screen -p 0 -X stuff "echo Uploading to Gdrive... $(printf \\r)"
     gdrive upload -p $gdrivefolderid $backupLocation/$serverName-$currentDay.tar.gz
-    screen -p 0 -X stuff "echo Upload complete! $(printf \\r)"
+    screen -p 0 -X stuff "echo Upload complete! Restarting $serverName...$(printf \\r)"
 fi
 
 elapsedTimeEnd="$(date -u +%s)"
-screen -p 0 -X stuff "echo Starting $serverName... $(printf \\r)"
+elapsed="$(($elapsedTimeEnd-$elapsedTimeStart))"
 screen -p 0 -X stuff "$startScript $(printf \\r)"
 
 if $restartOnly; then
-    echo "$serverName restarted on $currentDay!"
+    echo "$serverName restarted on $currentDay in $((elapsed/60))!"
 else
-    elapsed="$(($elapsedTimeEnd-$elapsedTimeStart/60))"
     compressedSize=$(du -sh $backupLocation* | cut -c 1-3)
     uncompressedSize=$(du -sh $fileToBackup* | cut -c 1-3)
-    echo "[$fileToBackup] ($uncompressedSize) successfully compressed to [$backupLocation] ($compressedSize) in $elapsed minutes!"
+    echo "[$fileToBackup] ($uncompressedSize) successfully compressed to [$backupLocation] ($compressedSize) in $((elapsed/60))!"
 fi
-
 exit 0
