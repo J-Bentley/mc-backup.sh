094e6ae310dd05f9eb444c7184b280d88de4d231
J-Bentley <jordanbentley77@gmail.com>
2023-06-04 11:49:50 -0400

    bug tested and working much better

--- mc-backup.sh
+++ mc-backup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 : '
 mc-backup by J-Bentley
-https://github.com/J-Bentley/mc-backup.sh'
+https://github.com/J-Bentley/mc-backup.sh
+Read the readme on github for setup, usage and how to automate.'
  
-serverDir="/home/jordan/minecraft-server"
-backupDir="/home/jordan/minecraft-backup"
+serverDir="/home/jordan/Minecraft-server"
+backupDir="/home/jordan/Minecraft-backup"
 startScript="bash start.sh"
 gracePeriod="1m"
 serverWorlds=("world" "world_nether" "world_the_end")
 # Don't change anything past this line unless you know what you're doing.
- 
-currentDay=$(date +"%Y-%m-%d")
-currentTime=$(date +"%H:%M")
+
+timeStamp=$(date +"[%Y-%m-%d-%H-%M]")
 screens=$(ls /var/run/screen/S-$USER -1 | wc -l || 0) # a file is created in /var/run/screen/S-$user for every screen session
 serverRunning=true
 worldsOnly=false
@@ -25,126 +25,122 @@ log () {
 }
 stopHandling () {
     # injects commands into screen via stuff to notify players, sleeps for graceperiod, stop server and sleeps for hdd spin times
-    log "[$currentDay] [$currentTime] Warning players & stopping server...\n"
-    screen -p 0 -X stuff "say Server is restarting in $gracePeriod!$(printf \\r)"
+    log "$timeStamp Warning players and stopping server..."
+    screen -p 0 -X stuff "say The server is restarting in $gracePeriod.$(printf \\r)"
     sleep $gracePeriod
-    screen -p 0 -X stuff "say Server is restarting now!$(printf \\r)"
+    screen -p 0 -X stuff "say The server is restarting now.$(printf \\r)"
     screen -p 0 -X stuff "save-all$(printf \\r)"
     sleep 5
     screen -p 0 -X stuff "stop$(printf \\r)"
     sleep 5
 }
-worldfoldercheck () {
+worldfolderCheck () {
     # Checks to make sure all the worlds defined in serverWorlds array exist as directories in serverDir
     for item in "${serverWorlds[@]}"
     do
         if [ ! -d $serverDir/$item ]; then
-            log "[$currentDay] [$currentTime] Error: World folder not found! Backup has been cancelled. ($serverDir/$item doesnt exist)\n"
+            log "$timeStamp Error: World folder not found! Backup has been cancelled. ($serverDir/$item doesnt exist)\n"
             exit 1
-	fi
+	    fi
     done
 }
- 
-# USER INPUT
+
+if [ $# -gt 1 ]; then
+    log -e "$timeStamp Error: Too many arguments! Backup has been cancelled.\n"
+    exit 1
+fi
+
+if [ $# -eq 0 ]; then
+    log "$timeStamp Full backup started."
+fi
+
 while [ $# -gt 0 ];
 do
     case "$1" in
       -h|--help)
-        echo -e "\nmc-backup by J-Bentley\nA local backup script for Minecraft!"
-        echo -e "\nUsage:\nNo args | Full backup\n-h | Help\n-w | Backup worlds only\n-r | Restart server with in-game warnings, no backup.\n-p | Backup plugins only.\n-pc | Backup plugin configs only."
+        echo -e "\nmc-backup by J-Bentley\nA local backup script for Minecraft servers!"
+        echo -e "\nUsage:\nNo args | Full backup\n-h | Help\n-w | Backup worlds only\n-r | Restart server with in-game warnings, no backup.\n-p | Backup plugins only.\n-pc | Backup plugin configs only.\n"
         exit 0
         ;;
       -w|--worlds)
-        worldfoldercheck
+        log "$timeStamp Worlds backup started."
+        worldfolderCheck
         worldsOnly=true
         ;;
       -p|--plugin)
+        log "$timeStamp Plugins backup started."
         pluginOnly=true
         ;;
       -r|--restart)
+        log "$timeStamp Restarting server."
         restartOnly=true
         ;;
       -pc|--pluginconfig)
+        log "$timeStamp Plugin configs backup started."
         pluginconfigOnly=true
         ;;
       *)
-      log -e "[$currentDay] [$currentTime] Error: Invalid argument: ${1}\n" 
+      log -e "$timeStamp Error: Invalid argument! (${1}) Backup has been cancelled.\n"
+      exit 1
       ;;
     esac
     shift
 done
  
-# Logs error and cancels script if too many args given to script
-if [ $# -gt 1 ]; then
-    log -e "[$currentDay] [$currentTime] Error: Too many arguments! Backup has been cancelled.\n"
-    exit 1
-fi
 # Logs error and cancels script if serverDir isn't found
 if [ ! -d $serverDir ]; then
-    log "[$currentDay] [$currentTime] Error: Server folder not found! Backup has been cancelled. ($serverDir doesnt exist)\n"
+    log "$timeStamp Error: Server folder not found! Backup has been cancelled. ($serverDir doesnt exist)\n"
     exit 1
 fi
 # Logs error and cancels script if backupDir isn't found
 if [ ! -d $backupDir ]; then
-    log "[$currentDay] [$currentTime] Error: Backup folder not found! Backup has been cancelled. ($backupDir doesnt exist)\n"
+    log "$timeStamp Error: Backup folder not found! Backup has been cancelled. ($backupDir doesnt exist)\n"
     exit 1
 fi
-# Logs error if java process isn't running but will continue anyways
+# Logs error if java process (server) isn't running but will continue anyways
 if ! ps -e | grep -q "java"; then
-    log "[$currentDay] [$currentTime] Warning: Server is not running! Continuing without in-game warnings...\n"
+    log "$timeStamp Warning: Server is not running! Continuing anyways..."
     serverRunning=false
 fi
- # Logs error if no screen sessions or more than one are running
+# Logs error if no screen sessions or more than one are running
 if [ $screens -eq 0 ]; then
-    log "[$currentDay] [$currentTime] Error: No screen sessions running! Backup has been cancelled.\n"
+    log "$timeStamp Error: No screen sessions running! Backup has been cancelled.\n"
     exit 1
 elif [ $screens -gt 1 ]; then
-    log "\n[$currentDay] [$currentTime] Error: More than 1 screen session is running! Backup has been cancelled.\n"
+    log "$timeStamp Error: More than 1 screen session is running! Backup has been cancelled.\n"
     exit 1
 fi
-# Wont execute stopHandling if server is offline upon script start
+
+# Wont warn players and stop server if the server is already offline upon script launch
 if $serverRunning; then
     stopHandling
 fi
  
-# LOGIC HANDLING
 if $restartOnly; then
-    log "[$currentDay] [$currentTime] Restarting server...\n"
+    :
 elif $worldsOnly; then
-    log "[$currentDay] [$currentTime] Worlds backup started...\n"
     # Starts the tar with files from the void (/dev/null is a symlink to a non-existent dir) so that multiple files can be looped in from array then gziped
-    tar cf $backupDir/WorldBackup-$currentDay.tar --files-from /dev/null 
+    tar cf $backupDir/$timeStamp-WorldsBackup.tar --files-from /dev/null 
     for item in "${serverWorlds[@]}"
     do
-        tar rf $backupDir/WorldBackup-$currentDay.tar "$serverDir/$item"
+        tar rf $backupDir/$timeStamp-WorldsBackup.tar "$serverDir/$item" &> /dev/null # Mutes tar output
     done
-    gzip $backupDir/WorldBackup-$currentDay.tar
+    gzip $backupDir/$timeStamp-WorldsBackup.tar
+    log "$timeStamp Created world backup."
 elif $pluginOnly; then
-    log "[$currentDay] [$currentTime] Plugins backup started...\n"
-    tar -czPf $backupDir/PluginsBackup-$currentDay.tar.gz $serverDir/plugins
+    tar -czPf $backupDir/$timeStamp-PluginsBackup.tar.gz $serverDir/plugins
+    log "$timeStamp Created plugins backup."
 elif $pluginconfigOnly; then
-    log "[$currentDay] [$currentTime] Plugin configs backup started...\n"
-    tar -czPf $backupDir/PluginConfigBackup-$currentDay.tar.gz --exclude='*.jar' $serverDir/plugins
+    tar -czPf $backupDir/$timeStamp-PluginConfigsBackup.tar.gz --exclude='*.jar' $serverDir/plugins
+    log "$timeStamp Created plugin configs backup."
 else
-    log "[$currentDay] [$currentTime] Full backup started...\n"
-    tar -czPf $backupDir/FullBackup-$currentDay.tar.gz $serverDir
-fi
-
-if $restartOnly; then
-    : # do nothing to avoid spam
-elif $worldsOnly; then
-    log "[$currentDay] [$currentTime] Created world backup.\n"
-elif $pluginOnly; then
-    log "[$currentDay] [$currentTime] Created plugin backup.\n"
-elif $pluginconfigOnly; then
-    log "[$currentDay] [$currentTime] Created plugin config backup.\n"
-else
-    log "[$currentDay] [$currentTime] Created full backup.\n"
+    tar -czPf $backupDir/$timeStamp-FullBackup.tar.gz $serverDir
+    log "$timeStamp Created full backup."
 fi
  
-# Will restart server if it was online upon script start OR if in restartOnly mode; wont restart server if it was already offline upon script launch unless restartOnly
+# If the server was offline upon script launch, WONT restart it UNLESS in restartOnly mode
 if $serverRunning || $restartOnly; then
     screen -p 0 -X stuff "$startScript $(printf \\r)"
-    log "[$currentDay] [$currentTime] Ran server start script.\n"
+    log "$timeStamp Ran server start script.\n"
 fi
 exit 0
